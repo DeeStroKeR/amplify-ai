4644d317f2dfbe9935824a0ae8b5079e551ed666
Anton Stroka <astroka@perfsys.com>
2025-11-12 17:19:20 +0100

    try sonnet 45

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,20 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+backend.auth.resources.authenticatedUserIamRole.addToPrincipalPolicy(
+  new PolicyStatement({
+    actions: ['bedrock:InvokeModel', 'bedrock:InvokeModelWithResponseStream'],
+    resources: ['arn:aws:bedrock:*:*:inference-profile/*'],
+  })
+);
